491e60e050673f45cde99c90683a82675de42652
JohnsonMelvin <jmelvinmelu113@gmail.com>
2024-03-20 11:57:05 +0530

    Update pool.js

--- pool.js
+++ pool.js
@@ -1,8 +1,17 @@
 const mysql = require('mysql2/promise');
+
+// const config = {
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'Jroot',
+//   database: 'BCP_Dashboard'
+// };
+
 const config = {
-  host: 'localhost',
-  user: 'root',
-  password: 'Jroot',
-  database: 'BCP_Dashboard'
-};
+    host: '14.194.141.107',
+    user: 'root',
+    password: 'root',
+    database: 'BCP_Dashboard'
+  };
+  
 module.exports = mysql.createPool(config);
