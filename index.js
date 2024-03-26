61cd42adebd7ef1f4c50e5859a7efa89aed96bb3
Sandhya <sandhya.k@tringapps.net>
2024-03-26 12:44:48 +0530

    forget-password

--- index.js
+++ index.js
@@ -16,19 +16,22 @@ app.use(session({
   cookie: { secure: false, maxAge: 24 * 60 * 60 * 1000 } // expires after 24 hours
 }));
 
+// Importing the forgot-password router
+const forgotPasswordRouter = require('./forgot-password');
+
+// Mounting the forgot-password router
+app.use('/forgot-password', forgotPasswordRouter);
+
+// Your other routes
 app.use('/login', require('./login'));
 app.use('/register', require('./register'));
 app.use('/logout', require('./logout'));
 app.use('/reset-password', require('./reset-password'));
-app.use('/forgot-password', require('./forgot-password'));
 app.use('/data', require('./data'));
 app.use('/bulk-upload', require('./bulk-upload'));
 app.use('/UserData', require('./UserData'));
 app.use('/update', require('./update'));
 app.use('/delete', require('./delete'));
-
-
-
 
 // Start the server
 app.listen(port, () => {
