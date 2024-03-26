74fb8c7ae140e25969e40fcc5f3e9488dee8210f
Sandhya <sandhya.k@tringapps.net>
2024-03-26 16:55:27 +0530

    smtp

--- reset-password.js
+++ reset-password.js
@@ -2,39 +2,38 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const pool = require('./pool');
-const bodyParser = require('body-parser');
 
 router.post('/', async (req, res) => {
-  if (!req.session.userId) {
-    return res.status(401).json({ message: 'Unauthorized. Please log in first.' });
-  }
-
-  if (!req.session.userName || !newPassword) {
-    return res.status(400).json({ message: 'UserName and new password are required' });
-  }
+  const { resetToken, newPassword } = req.body;
 
   try {
-    // Check if the user exists in the database and if the session user matches the database user
-    const [rows] = await pool.query('SELECT * FROM users WHERE UserName = ?', [req.session.userName]);
-    if (rows.length === 0 || rows[0].ID !== req.session.userId) {
-      return res.status(400).json({ message: 'User not found or session expired' });
+    const [user] = await pool.query('SELECT * FROM users WHERE resetToken = ?', [resetToken]);
+
+    if (!user || user.length === 0) {
+      return res.status(400).json({ message: 'Invalid or expired reset token' });
+    }
+
+    // Check if the reset token has expired
+    const currentTimestamp = Date.now();
+
+    if (user[0].resetTokenExpiry && currentTimestamp > user[0].resetTokenExpiry.getTime()) {
+            return res.status(400).json({ message: 'Reset token has expired' });
     }
 
     // Hash the new password
     const salt = await bcrypt.genSalt();
     const newPasswordHash = await bcrypt.hash(newPassword, salt);
 
-    // Update the user's password in the database using the user's ID
-    const updateQuery = 'UPDATE users SET Password = ?, Salt = ? WHERE ID = ?';
-    await pool.query(updateQuery, [newPasswordHash, salt, rows[0].ID]);
+    // Update the user's password in the database
+    await pool.query('UPDATE users SET PasswordHash = ?, Salt = ?, resetToken = NULL, resetTokenExpiry = NULL WHERE UserID = ?', [newPasswordHash, salt, user[0].UserID]);
 
-    console.log("Password reset successfully!");
-    res.status(200).json({ message: 'Password reset successfully' });
+    // Respond with success message
+    return res.status(200).json({ message: 'Password reset successfully' });
+
   } catch (error) {
-    console.error("Error resetting password:", error);
-    res.status(500).json({ message: 'Error resetting password' });
+    console.error('Error resetting password:', error);
+    return res.status(500).json({ message: 'Error resetting password' });
   }
 });
 
 module.exports = router;
-
