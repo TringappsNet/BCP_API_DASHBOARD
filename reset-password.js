f8bda0bca61289ccc5a091940eebec510d0d6efd
Sandhya <sandhya.k@tringapps.net>
2024-04-01 11:28:26 +0530

    Changes in reset password

--- reset-password.js
+++ reset-password.js
@@ -14,18 +14,16 @@ router.post('/', async (req, res) => {
     }
 
     // Check if the reset token has expired
-    const currentTimestamp = Date.now();
-
-    if (user[0].resetTokenExpiry && currentTimestamp > user[0].resetTokenExpiry.getTime()) {
-            return res.status(400).json({ message: 'Reset token has expired' });
-    }
+    // if (user[0].resetTokenExpiry && new Date() > user[0].resetTokenExpiry) {
+    //   return res.status(400).json({ message: 'Reset token has expired' });
+    // }
 
     // Hash the new password
     const salt = await bcrypt.genSalt();
     const newPasswordHash = await bcrypt.hash(newPassword, salt);
 
     // Update the user's password in the database
-    await pool.query('UPDATE users SET PasswordHash = ?, Salt = ?, resetToken = NULL, resetTokenExpiry = NULL WHERE UserID = ?', [newPasswordHash, salt, user[0].UserID]);
+    await pool.query('UPDATE users SET PasswordHash = ?, Salt = ?, resetToken = NULL, resetTokenExpiry = NULL WHERE resetToken = ?', [newPasswordHash, salt, resetToken]);
 
     // Respond with success message
     return res.status(200).json({ message: 'Password reset successfully' });
