dd7563a604517ecd0d3531b8d5f20df5a7bc9385
JohnsonMelvin <jmelvinmelu113@gmail.com>
2024-04-02 10:47:38 +0530

    Users update, org creation, users fetc

--- Users.js
+++ Users.js
@@ -4,8 +4,14 @@ const pool = require('./pool');
 
 router.get('/', async (req, res) => {
   try {
-    // Query the database to retrieve all users
-    const [rows] = await pool.query('SELECT UserName, Organization, Email, Role FROM Users');
+    // Query the database to retrieve user information along with organization name and role
+    const query = `
+      SELECT u.UserName, o.org_name AS Organization, u.Email, r.role AS Role
+      FROM users u
+      LEFT JOIN organization o ON u.Org_ID = o.org_ID
+      LEFT JOIN role r ON u.Role_ID = r.role_ID
+    `;
+    const [rows] = await pool.query(query);
     
     // Send back the array of user information
     res.status(200).json(rows);
