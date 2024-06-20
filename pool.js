8c8387f82686946b75ca0529f18cdd4be8ff365b
JohnsonMelvin <126421095+JohnsonMelvin@users.noreply.github.com>
2024-06-20 13:05:20 +0530

    Update pool.js

--- pool.js
+++ pool.js
@@ -1,47 +1,13 @@
 const mysql = require('mysql2/promise');
 
-// const config = {
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'Jroot',
-//   database: 'BCP_Database'
-// };
-
-
-// const config = {
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'root',
-//     database: 'bcp'
-//   };
-
-
-
-// const config = {
-//   host: '192.168.1.50',
-//   user: 'root',
-//   password: 'root',
-//   database: 'BCP_Dashboard'
-// };
-
-  
-
-// const config = {
-//   host: '192.168.1.50',
-//   user: 'root',
-//   password: 'root',
-//   database: 'BCP_Database'
-  
-// };
-
-
 const config = {
-  host: 'database-1.c3qaws60khba.us-east-2.rds.amazonaws.com',
-  user: 'admin',
+  host: '',
+  user: 'bcpadmin',
   port: '3306',
-  password: '#b3nf0rd2024',
-  database: 'bcp'
-  
+  password: '',
+  database: 'bcp',
+  ssl: {
+    mode: 'require'
+  }
 };
-
 module.exports = mysql.createPool(config);
