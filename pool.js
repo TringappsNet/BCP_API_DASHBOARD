8747596c25eef0c547b43f8fe5624d67fb83b6ba
JohnsonMelvin <126421095+JohnsonMelvin@users.noreply.github.com>
2024-06-22 19:28:47 +0530

    azure pool

--- pool.js
+++ pool.js
@@ -1,47 +1,15 @@
 const mysql = require('mysql2/promise');
-
-// const config = {
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'Jroot',
-//   database: 'BCP_Database'
-// };
-
-
-// const config = {
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'root',
-//     database: 'bcp'
-//   };
-
-
-
-// const config = {
-//   host: '192.168.1.50',
-//   user: 'root',
-//   password: 'root',
-//   database: 'BCP_Dashboard'
-// };
-
-  
-
-// const config = {
-//   host: '192.168.1.50',
-//   user: 'root',
-//   password: 'root',
-//   database: 'BCP_Database'
-  
-// };
-
+const fs = require('fs');
 
 const config = {
-  host: 'database-1.c3qaws60khba.us-east-2.rds.amazonaws.com',
-  user: 'admin',
-  port: '3306',
-  password: '#b3nf0rd2024',
-  database: 'bcp'
-  
+  host: 'bcpbackendnew.mysql.database.azure.com',
+  user: 'bcpadmin',
+  password: 'B63ntf0rdC@p',
+  database: 'bcp',
+  port: 3306,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: fs.readFileSync('./certs/certificate1.pem')  }
 };
 
 module.exports = mysql.createPool(config);
