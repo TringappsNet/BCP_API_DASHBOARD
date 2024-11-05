f5af199cdcf7281673271ed63c2d91339f85b128
Dinesh <85795537+dinesh-git07@users.noreply.github.com>
2024-11-05 12:25:01 +0530

    configured the prod db

--- pool.js
+++ pool.js
@@ -2,15 +2,15 @@ const mysql = require('mysql2/promise');
 const fs = require('fs');
 
 const config = {
-  host: 'localhost',
-  user: 'root',
-  password: 'Dinesh_2002',
+  host: 'bcpbackendnew.mysql.database.azure.com',
+  user: 'bcpadmin',
+  password: 'B63ntf0rdC@p',
   database: 'bcp',
   port: 3306,
-  // ssl: {
-  //   rejectUnauthorized: true,
-  //   ca: fs.readFileSync('./certs/certificate1.pem'),
-  // },
+  ssl: {
+    rejectUnauthorized: true,
+    ca: fs.readFileSync('./certs/certificate1.pem'),
+  },
 };
 
 module.exports = mysql.createPool(config);
